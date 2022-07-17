a1dbbe4d684541fe8b85cfeb27642a15549ece6f
LEE SUNGWOOK <gbk1414@gmail.com>
2022-07-17 18:18:32 +0900

    fixing hidden

--- src/login.js
+++ src/login.js
@@ -6,11 +6,11 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
+  event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
-  event.preventDefault();
 }
 
 function Timecheck(nHour) {
@@ -39,8 +39,7 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
