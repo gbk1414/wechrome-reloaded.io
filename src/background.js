6bbd2917f10a152c1eb3fe026da06655821c946a
LEE SUNGWOOK <gbk1414@gmail.com>
2022-07-17 14:48:02 +0900

    styling finished

--- src/background.js
+++ src/background.js
@@ -1,4 +1,11 @@
-const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+const images = [
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg",
+  "img4.jpg",
+  "img5.jpg",
+  "img6.jpg",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
