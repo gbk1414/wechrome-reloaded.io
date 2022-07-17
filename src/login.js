93941e66539bc9c57a03003b3a50bc8c164d4314
LEE SUNGWOOK <gbk1414@gmail.com>
2022-07-17 18:14:12 +0900

    fixing not hidden add...

--- src/login.js
+++ src/login.js
@@ -6,11 +6,11 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
+  event.preventDefault();
 }
 
 function Timecheck(nHour) {
